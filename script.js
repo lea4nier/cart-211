a922c59947156b7023e4a6e201161d14138cbbaa
Léa Fournier <lea.fournier@gmail.com>
2023-10-23 14:49:50 -0400

    week 6 class

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    document.getElementById("theParagraph").innerHTML = "Hello World";
+    document.getElementById("sticky").src = "https://www.clipartmax.com/png/small/8-86768_clip-arts-related-to-hello-kitty-and-batman.png";
+    document.getElementById("theParagraph").style.color = "#11f5f1"; 
+    document.getElementById("theParagraph").style.backgroundColor = "#ff55a3"; 
+}           
