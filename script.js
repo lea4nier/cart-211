764b573d5f3bb466aaebaf23e8f279e112949b7b
Léa Fournier <lea.fournier@gmail.com>
2023-10-28 17:07:35 -0400

    week 7 reading response

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function myFunction(){
     document.getElementById("theParagraph").innerHTML = "Hello World";
-    document.getElementById("sticky").src = "https://www.clipartmax.com/png/small/8-86768_clip-arts-related-to-hello-kitty-and-batman.png";
+    document.getElementById("sticky").src = "https://imgix.ranker.com/user_node_img/50023/1000444553/original/pups-in-a-bucket-of-leaves-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375";
     document.getElementById("theParagraph").style.color = "#11f5f1"; 
     document.getElementById("theParagraph").style.backgroundColor = "#ff55a3"; 
 }           
